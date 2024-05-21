fc4c7c753aa90c04e5537573035299916d42b029
kiwifruit47 <victoria.dimitrovax@icloud.com>
2024-05-21 23:59:59 +0300

    fixed bug in the username and password input fields

--- src/Login.jsx
+++ src/Login.jsx
@@ -14,9 +14,9 @@ export const Login = (props) => {
             <h1>Login</h1>
             <form className="login-form">
                 <label htmlFor="username">Username</label>
-                <input value={username} onChange={(e) => setPass(e.target.value)} type="text" name="username" placeholder="JohnDoe123" id="username"/>
+                <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" name="username" placeholder="JohnDoe123" id="username"/>
                 <label htmlFor="password">Password</label>
-                <input value={pass} onChange={(e) => setUsername(e.target.value)} type="password" name="password" placeholder="**********" id="password"/>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" name="password" placeholder="**********" id="password"/>
                 <button className="btn-main">Login</button>
             </form>
             <button onClick={() => props.onFormSwitch('register')}>Don't have an account? Register</button>
--- src/Register.jsx
+++ src/Register.jsx
@@ -17,10 +17,10 @@ export const Register = (props) => {
             <h1>Register</h1>
             <form className="register-form">
                 <label htmlFor="username">Username</label>
-                <input value={username} onChange={(e) => setPass(e.target.value)} type="text" name="username" placeholder="JohnDoe123" id="username"/>
+                <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" name="username" placeholder="JohnDoe123" id="username"/>
                 <small id="usernameHelpBlock">Username should contain only letters and numbers.</small>
                 <label htmlFor="password">Password</label>
-                <input value={pass} onChange={(e) => setUsername(e.target.value)} type="password" name="password" placeholder="**********" id="password"/>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" name="password" placeholder="**********" id="password"/>
                 <small id="passwordHelpBlock">Password should contain only letters and numbers.</small>
                 <label htmlFor="fullName">Full Name</label>
                 <input value={fullName} onChange={(e) => setFullName(e.target.value)} type="text" name="fullName" placeholder="John Doe" id="fullName"/>
