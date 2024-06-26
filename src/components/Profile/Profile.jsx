5f8e472d88e1cbc133cac0cb5c3f43a02044e5c2
kiwifruit47 <victoria.dimitrovax@icloud.com>
2024-06-26 12:56:21 +0300

    Added react query and fixed Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,64 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axiosInstance from '../../api';
 import { IoPersonCircle } from "react-icons/io5";
+import { Hourglass } from 'react-loader-spinner';
+import "./Profile.css";
 
-const style = { color: "#013B67", fontSize: "7em" }
+const userId = 1;
 
 const Profile = () => {
+  const { data: userData, error, isLoading } = useQuery({
+    queryKey: ["user", userId],
+    queryFn: async () => {
+      const res = await axiosInstance.get(`/users/${userId}`);
+      return res.data;
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <div style={{ textAlign: "center", paddingTop: "20rem" }}>
+        <Hourglass
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="hourglass-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          colors={['#306cce', '#72a1ed']}
+        />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div className="error">Error: {error.message}</div>;
+  }
+
+  let userRole = userData?.roleType;
+  let userRoleForHeadingElement = userRole?.charAt(0).toUpperCase() + userRole?.slice(1);
+
   return (
-    <div>
-        <IoPersonCircle style={style}/>
+    <div className="profile">
+      <div className='pfp'>
+        <IoPersonCircle style={{ color: "#013B67", fontSize: "7em" }} />
+      </div>
+      <section className='user-information'>
+        <h2>User Information</h2>
+        <label htmlFor="firstName">First Name: </label>
+        <span>{userData?.firstName}</span><br />
+        <label htmlFor="lastName">Last Name: </label>
+        <span>{userData?.lastName}</span><br />
+        <label htmlFor="role">Role: </label>
+        <span>{userData?.roleType}</span><br />
+      </section>
+      <section className='role-information'>
+        <h2>{userRoleForHeadingElement} Information</h2>
+        
+      </section>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,41 +1,61 @@
-import React, { useState } from "react"
+import React, { useState, useRef, useEffect } from "react"
 
 const Login = (props) => {
+    const userRef = useRef();
+    const errorRef = useRef();
+
     const [username, setUsername] = useState('');
     const [pass, setPass] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, []);
+
+    useEffect(() => {
+        setErrorMsg('');
+    }, [username, pass]);
 
     // const handleSubmit = (e) => {
     //     e.preventDefaut();
     //     console.log(username);
     // }
     const handleSubmit = () => {
+        e.preventDefaut();
         setIsAuthenticated(true);
       }
  
     return (
-        <div role="form" className="auth-form-container">
-            <h1>Login</h1>
-            <form className="login-form">
-                <label htmlFor="username">Username</label>
-                <input 
-                value={username} 
-                onChange={(e) => setUsername(e.target.value)} 
-                type="text" 
-                name="username" 
-                placeholder="JohnDoe123" 
-                id="username"/>
-                <label htmlFor="password">Password</label>
-                <input 
-                value={pass} 
-                onChange={(e) => setPass(e.target.value)} 
-                type="password" 
-                name="password" 
-                placeholder="**********" 
-                id="password"/>
-                <button onClick={handleSubmit} className="btn-main">Login</button>
-            </form>
-            <span style={{marginTop: "1.5rem"}}>Don't have an accout? Contact school administration.</span>
-        </div>
+        <>
+            <p ref={errorRef} className={errorMsg ? "error-msg" : "offscreen"} aria-live="assertive">{errorMsg}</p>
+            <div role="form" className="auth-form-container">
+                <h1>Login</h1>
+                <form className="login-form">
+                    <label htmlFor="username">Username</label>
+                    <input 
+                    value={username} 
+                    onChange={(e) => setUsername(e.target.value)} 
+                    type="text" 
+                    name="username" 
+                    placeholder="JohnDoe123" 
+                    id="username"
+                    ref={userRef}
+                    required
+                    autoComplete="off"/>
+                    <label htmlFor="password">Password</label>
+                    <input 
+                    value={pass} 
+                    onChange={(e) => setPass(e.target.value)} 
+                    type="password" 
+                    name="password" 
+                    placeholder="**********" 
+                    id="password"
+                    required/>
+                    <button onClick={handleSubmit} className="btn-main">Login</button>
+                </form>
+                <span style={{marginTop: "1.5rem"}}>Don't have an accout? Contact school administration.</span>
+            </div>
+        </>
     )
 }
 
