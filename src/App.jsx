823c5377df1ce9ed19990e67e92f4a5af79934a4
agenta007 <victoria.dimitrovax@icloud.com>
2025-12-07 23:41:39 +0200

    finished sidebar for all roles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { AuthProvider } from './context/AuthProvider';
 import AppRouter from './router/AppRouter';
 
 function App() {
 
   return (
-    <>
-      <AuthProvider>
         <AppRouter/>
-      </AuthProvider>
-    </>
   )
 }
 
