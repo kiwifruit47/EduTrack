1a3235075c9c58fd65ea96a69a767302bbdf2577
agenta007 <victoria.dimitrovax@icloud.com>
2026-03-15 23:10:40 +0200

    added backend dir

---
+++ backend/e-journal/src/main/java/com/edutrack/e_journal/EJournalApplication.java
@@ -0,0 +1,13 @@
+package com.edutrack.e_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EJournalApplication.class, args);
+	}
+
+}
---
+++ backend/e-journal/src/test/java/com/edutrack/e_journal/EJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutrack.e_journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
