3c05de23c1b101e6113d855aaf9848525ec12501
agenta007 <stati_kosev@abv.bg>
2026-03-20 11:04:05 +0200

    addedd studentrepository.java and .class

---
+++ backend/e-journal/src/main/java/com/edutrack/e_journal/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.edutrack.e_journal.repository;
+
+import com.edutrack.e_journal.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findAllBySchool_Id(Long schoolId);
+}
