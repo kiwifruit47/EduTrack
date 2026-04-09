4c32ea0d62999d92dd3c481ef60571bd623486a0
agenta007 <chovekutshema@proton.me>
2026-04-09 08:45:34 +0300

    checkpoint

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.REACT_APP_BASE_URL,
+  baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
   withCredentials: true,
 });
 
